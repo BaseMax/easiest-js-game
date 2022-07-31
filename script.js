1e95ae9e3a78fc2d48433814e873e29f93632477
Max Base <MaxBaseCode@Gmail.Com>
2022-07-31 17:12:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const character = document.querySelector("#character");
+// Const variables, elements
 const block = document.querySelector("#block");
+const character = document.querySelector("#character");
+const score = document.getElementById("scoreSpan")
 
+// Variable
 let counter = 0;
 
+// Function
 const jump = () => {
     if (character.classList === "animate") return;
     character.classList.add("animate");
-    
+
     setTimeout(() => {
         character.classList.remove("animate");
     }, 300);
 };
 
-const checkDead = setInterval(() => {
+// Init, timer
+setInterval(() => {
     const characterTop = parseInt( window.getComputedStyle(character).getPropertyValue("top") );
     const blockLeft = parseInt( window.getComputedStyle(block).getPropertyValue("left") );
 
     if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+        counter = 0;
         block.style.animation = "none";
         alert("Game score: " + Math.floor(counter / 100));
-        counter = 0;
-        block.style.animation = "block 1s infinite linear";
     } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+        score.innerHTML = Math.floor(counter / 100);
     }
 }, 10);
